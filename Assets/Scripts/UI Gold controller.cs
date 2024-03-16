afef857e8770da623dd93fdd0975095749f8842a
HanchGG <emil.rakhimov.designer@gmail.com>
2024-03-17 00:57:34 +0300

    Ready

--- Assets/Scripts/UI Gold controller.cs
+++ Assets/Scripts/UI Gold controller.cs
@@ -12,13 +12,31 @@ public class UIGoldcontroller : MonoBehaviour
     public Animator Anim;
     float Timer = 0;
     public GameObject Sound;
+    public GameObject Wheel1;
+    public GameObject Wheel2;
+    public GameObject Wheel3;
+    public GameObject Wheel4;
+    public GameObject Wheel5;
+    public GameObject Wheel6;
+    public GameObject WheelsSound;
+    public GameObject WithoutCar;
 
     private void Start()
     {
         Anim = gameObject.GetComponent<Animator>();
         Sound.SetActive(false);
+        WheelsSound.SetActive(false);
+        WithoutCar.SetActive(false);
     }
-
+    private void Wheels()
+    {
+        Wheel1.transform.Rotate(0, 0, -0.5f);
+        Wheel2.transform.Rotate(0, 0, -0.5f);
+        Wheel3.transform.Rotate(0, 0, -0.5f);
+        Wheel4.transform.Rotate(0, 0, 0.5f);
+        Wheel5.transform.Rotate(0, 0, 0.5f);
+        Wheel6.transform.Rotate(0, 0, 0.5f);
+    }
     public void Update()
     {
         GoldCount.text = _goldCount.ToString();
@@ -27,8 +45,12 @@ public class UIGoldcontroller : MonoBehaviour
         {
             Timer += Time.deltaTime;
             Anim.SetBool("isStart", true);
+            Wheels();
+            WheelsSound.SetActive(true);
             if (Timer >= 17.3f)
             {
+                WithoutCar.SetActive(true);
+                WheelsSound.SetActive(false);
                 gameObject.SetActive(false);
             }
         }
